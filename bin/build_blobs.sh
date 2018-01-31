a72021730e64b55a9fc5c8d35851897a7253805f
Robert Kent <git@rooted.ltd.uk>
2018-01-31 12:22:38 +0000

    Add set -e

--- bin/build_blobs.sh
+++ bin/build_blobs.sh
@@ -3,10 +3,11 @@
 # This assumes Bosh-S3CLI has been checked out from https://github.com/cloudfoundry/bosh-s3cli to go/src/github.com/cloudfoundry/bosh-s3cli
 #
 
+set -e
+
 export GOPATH="$PWD/build/go"
 
-mkdir -p "$GOPATH"
-mkdir -p "$PWD/blobs"
+mkdir -p "$GOPATH" blobs
 
 cd build/go/src/github/cloudfoundry/bosh-s3cli
 
--- bin/download_blobs.sh
+++ bin/download_blobs.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 POSTGRESQL_VERSION='9.6.6'
 
 curl -SOC - "https://ftp.postgresql.org/pub/source/v$POSTGRESQL_VERSION/postgresql-$POSTGRESQL_VERSION.tar.gz"
