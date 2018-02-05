11806af1712715f566b21e3de5fc4d154f77822f
Robert Kent <git@rooted.ltd.uk>
2018-02-05 15:54:21 +0000

    Create missing blob dir

--- bin/download_blobs.sh
+++ bin/download_blobs.sh
@@ -4,4 +4,8 @@ set -e
 
 POSTGRESQL_VERSION='9.6.6'
 
+[ -d blobs ] || mkdir -p blobs
+
+cd blobs
+
 curl -SOC - "https://ftp.postgresql.org/pub/source/v$POSTGRESQL_VERSION/postgresql-$POSTGRESQL_VERSION.tar.gz"
